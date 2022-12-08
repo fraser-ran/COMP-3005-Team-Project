aa763272ca1601fe6749519728128c651fc4e688
fraser-ran <93053986+fraser-ran@users.noreply.github.com>
2022-12-08 15:30:12 -0500

    Added a get price function and more
    Added functions that allow you to get prices either from a singular isbn or from an array of isbns

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from sqlFunc import *
 
 if __name__ == "__main__":
     # addPublisher(1, "test Publisher", "test address","emai", "number","1234")
-    # insertBook(15651, "title","test Publisher", "author", "genre", 100, 10)
+    insertBook(2, "book with ti","test Publisher", "author", "action", 200, 20)
     print(searchLikeName('ti'))
+    print(getPriceIsbn(2))
     pass
 
 
