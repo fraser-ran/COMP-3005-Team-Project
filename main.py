cbb63f84cb3859613d89b80df65d19d7a63c4940
vash <vashiran.v@gmail.com>
2022-12-10 15:35:16 -0500

    Add/Delete Button implemented, Sales table fixed

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from sqlFunc import *
 
 if __name__ == "__main__":
     # addPublisher(1, "test Publisher", "test address","emai", "number","1234")
-    # insertBook(69, "title","test Publisher", "author", "action", 200, 20,10)
-    print(searchLikeName('book'))
+    insertBook(69, "title","test Publisher", "author", "action", 200, 20,10)
+    insertBook(68, "title2","test Publisher", "author", "action", 200, 20,10)
+    #print(searchLikeName('book'))
     # print(getPriceIsbn(2))
     pass
 
